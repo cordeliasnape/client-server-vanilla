a124cf671c56ea51144be3b54020818ec0cc6542
cordelia <snapecordelia@gmail.com>
2024-07-23 15:40:04 +0100

    changed localhost link

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 const posts = document.getElementById("posts");
 
 async function fetchPosts() {
-  let response = await fetch("http://localhost:3001/posts");
+  let response = await fetch("https://server-vanilla.onrender.com/posts");
   let allPosts = await response.json();
   console.log(allPosts);
 
